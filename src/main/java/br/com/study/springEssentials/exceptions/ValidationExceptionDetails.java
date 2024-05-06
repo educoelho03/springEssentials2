74502b82cefeecf2c046bbf89d6a0997ebcabe6c
ecoelho03 <ecoelho@contanciainvest.com.br>
2024-05-06 14:14:42 -0300

    added new exceptions

--- src/main/java/br/com/study/springEssentials/exceptions/ValidationExceptionDetails.java
+++ src/main/java/br/com/study/springEssentials/exceptions/ValidationExceptionDetails.java
@@ -3,7 +3,10 @@ package br.com.study.springEssentials.exceptions;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
+import java.util.List;
+
 @Getter
 @SuperBuilder
-public class BadRequestExceptionDetails extends ExceptionDetails{
+public class ValidationExceptionDetails extends ExceptionDetails{
+    List<FieldMessage> fieldErrors;
 }
