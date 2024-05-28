598a41342e54c94f4dc004d9e693c984703144b0
ecoelho03 <ecoelho@contanciainvest.com.br>
2024-05-28 10:34:58 -0300

    adicionado security em metodos especificos

--- src/main/java/br/com/study/springEssentials/exceptions/BadRequestExceptionDetails.java
+++
@@ -1,9 +0,0 @@
-package br.com.study.springEssentials.exceptions;
-
-import lombok.Getter;
-import lombok.experimental.SuperBuilder;
-
-@Getter
-@SuperBuilder
-public class BadRequestExceptionDetails extends ExceptionDetails{
-}
