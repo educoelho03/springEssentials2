0efaec5990646b7786ac65e06b6ab2d45bff6b1c
ecoelho03 <ecoelho@contanciainvest.com.br>
2024-05-28 10:36:36 -0300

    adicionado security em metodos especificos

---
+++ spring-boot-41-seguranca-a-nivel-de-metodos-com-preAuthori/springEssentials2/src/main/java/br/com/study/springEssentials/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package br.com.study.springEssentials.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())  // significa que qualquer requisição precisam passar por autorização
+                .httpBasic(withDefaults())
+                .csrf(csrf -> csrf.disable()
+//                .csrf((csrf) -> csrf
+//                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) // é necessario estar false para o front conseguir ler esse token
+                );
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user = User.withUsername("edu")
+                .password(passwordEncoder.encode("java"))
+                .roles("USER", "ADMIN")
+                .build();
+
+        UserDetails user2 = User.withUsername("fefe")
+                .password(passwordEncoder.encode("gata"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user, user2);
+    }
+
+}
---
+++ spring-boot-41-seguranca-a-nivel-de-metodos-com-preAuthori/springEssentials2/src/main/java/br/com/study/springEssentials/service/AnimeService.java
@@ -0,0 +1,57 @@
+package br.com.study.springEssentials.service;
+
+import br.com.study.springEssentials.domains.domain.Anime;
+import br.com.study.springEssentials.domains.mapper.AnimeMapper;
+import br.com.study.springEssentials.exceptions.BadRequestException;
+import br.com.study.springEssentials.repository.AnimeRepository;
+import br.com.study.springEssentials.domains.requests.AnimePostRequestBody;
+import br.com.study.springEssentials.domains.requests.AnimePutRequestBody;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AnimeService {
+
+    private final AnimeRepository animeRepository;
+    private final AnimeMapper animeMapper;
+
+    public Page<Anime> listAll(Pageable pageable){
+        return animeRepository.findAll(pageable);
+    }
+
+    public List<Anime> listAllNonPageable(){
+        return animeRepository.findAll();
+    }
+
+    public Anime findByIdOrThrowBadRequestException(long id){
+        return animeRepository.findById(id)
+                .orElseThrow(() -> new BadRequestException("Anime not found"));
+    }
+
+    public List<Anime> findByName(String name){
+        return animeRepository.findByName(name);
+    }
+
+    @Transactional
+    public Anime save(AnimePostRequestBody animePostRequestBody) {
+         return animeRepository.save(animeMapper.toAnime(animePostRequestBody));
+    }
+
+    public void delete(Long id){
+        animeRepository.deleteById(id);
+    }
+
+    public void replace(AnimePutRequestBody animePutRequestBody){
+        Anime savedAnime = findByIdOrThrowBadRequestException(animePutRequestBody.getId());
+        Anime anime = animeMapper.toAnime(animePutRequestBody);
+        anime.setId(savedAnime.getId());
+
+        animeRepository.save(anime);
+    }
+}
