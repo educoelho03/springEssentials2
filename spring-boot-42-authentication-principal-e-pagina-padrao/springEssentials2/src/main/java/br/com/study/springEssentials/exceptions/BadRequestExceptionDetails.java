0f7a78c173f98d77839eebcd53f47a572616356e
ecoelho03 <ecoelho@contanciainvest.com.br>
2024-05-28 11:00:36 -0300

    add authenticação na pagina princiáç

---
+++ spring-boot-42-authentication-principal-e-pagina-padrao/springEssentials2/src/main/java/br/com/study/springEssentials/exceptions/BadRequestExceptionDetails.java
@@ -0,0 +1,9 @@
+package br.com.study.springEssentials.exceptions;
+
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+public class BadRequestExceptionDetails extends ExceptionDetails{
+}
